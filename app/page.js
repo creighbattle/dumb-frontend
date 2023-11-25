11a9cdcaa17cef96607f29e4c717194459fb3705
Creigh Battle <creighbattle@Creighs-MacBook-Pro.local>
2023-11-25 11:39:36 -0500

    remove head tags

--- app/page.js
+++ app/page.js
@@ -1,14 +1,5 @@
 import AskPage from "@/components/AskPage";
-import Head from "next/head";
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <meta name="theme-color" content="#32CD32" /> {/* Your desired color */}
-        {/* other head elements */}
-      </Head>
-      <AskPage />
-    </>
-  );
+  return <AskPage />;
 }
